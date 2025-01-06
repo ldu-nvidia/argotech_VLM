80e57169b98f8f9697044042c0e99b8ad4bdfc96
ldu <ldu@nvidia.com>
2025-01-06 15:20:51 -0800

    added code to run vlm inference, pending batch processing

--- main.py
+++ main.py
@@ -1,41 +1,67 @@
 from openai import OpenAI
-client = OpenAI(base_url="http://0.0.0.0:8000/v1", api_key="not-used")
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "text",
-                "text": "What is in this image?"
-            },
-            {
-                "type": "image_url",
-                "image_url": {
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-                }
-            }
-        ]
-    }
-]
-chat_response = client.chat.completions.create(
-    model="meta/llama-3.2-11b-vision-instruct",
-    messages=messages,
-    max_tokens=256,
-    stream=False
-)
-assistant_message = chat_response.choices[0].message
-print(assistant_message)
+import os
+import base64
+class AgroVLMAgent:
+    def __init__(self) -> None:
+        self.client = OpenAI(base_url="http://0.0.0.0:8000/v1", api_key="not-used")
+        self.prompts = ["can you tell what plant is this from the leaf?", "is the leaf healthy or not?", "what kind of disease do you think the leaf has?"]
+        self.image_directory = "/home/ldu/Repos/argotech_VLM/images/train/Apple___Apple_scab/"
+        self.model = "meta/llama-3.2-11b-vision-instruct"
+      
+    def stripPlantInfo(self, plant_string: str) -> tuple:
+        plant, condition = plant_string.split("___")[0], plant_string.split("___")[1]
+        return (plant, condition)
+    
+    def encodeImage(self, image_path: str) -> str:
+        with open(image_path, "rb") as f:
+            image_b64 = base64.b64encode(f.read()).decode()
+        return image_b64
+
+    def formMessage(self, role: str, text_prompt: str, image_b64: str):
+        return  [
+                    {
+                        "role": role,
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": text_prompt
+                            },
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": "data:image/jpeg;base64," + image_b64
+                                }
+                            }
+                        ]
+                    }
+                ]
+
+    def getVLMResponse(self, messages):
+        chat_response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            max_tokens=256,
+            stream=False
+        )
+        assistant_message = chat_response.choices[0].message
+        print(assistant_message)
+        return assistant_message
+
+def main():
+    vlm_agent = AgroVLMAgent()
+    plant, disease = vlm_agent.stripPlantInfo(vlm_agent.image_directory[:-1].split("/")[-1])
+    
+    one_image = vlm_agent.image_directory + os.listdir(vlm_agent.image_directory)[0]
+    print("Ground Truth: the plant is ", plant, " and the disease is ", disease)
+    print("Image directory is: ", one_image)
+    decoded_image = vlm_agent.encodeImage(one_image)
+    for prompt_idx in range(len(vlm_agent.prompts)):
+        prompt = vlm_agent.prompts[prompt_idx]
+        message = vlm_agent.formMessage(role="user", text_prompt=prompt, image_b64=decoded_image)
+        print("\n")
+        print("PROMPT: ", prompt)
+        vlm_agent.getVLMResponse(message)
 
 
-
-## alternative to pass images is to encode images into base64 format then send messages along with other params
-'''
-{
-    "type": "image_url",
-    "image_url": {
-        "url": "data:image/jpeg;base64,SGVsbG8gZGVh...ciBmZWxsb3chIQ=="
-    }
-}
-with open("image.png", "rb") as f:
-    image_b64 = base64.b64encode(f.read()).decode()
-'''
+if __name__ == "__main__":
+    main()
